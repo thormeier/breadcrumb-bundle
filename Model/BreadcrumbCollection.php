63fff2438956234cbe5c280bf781ae89809e13f8
Pascal Thormeier <pascal.thormeier@liip.ch>
2015-06-04 23:58:36 +0200

    Remove debugging output option

--- Model/BreadcrumbCollection.php
+++ Model/BreadcrumbCollection.php
@@ -100,10 +100,10 @@ class BreadcrumbCollection
      *
      * @return mixed
      */
-    private function getBreadcrumbPosition(Breadcrumb $breadcrumb, $dump = false)
+    private function getBreadcrumbPosition(Breadcrumb $breadcrumb)
     {
         $position = array_search($breadcrumb, $this->breadcrumbs);
-        if ($dump) { var_dump($position); exit; }
+
         if (false === $position) {
             throw new \InvalidArgumentException(sprintf(
                 'Breadcrumb for route "%s" with label "%s" not found',
